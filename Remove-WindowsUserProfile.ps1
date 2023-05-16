93978454d887ec321e5ee3ee448dd6c8579f925e
egiberne <29532199+egiberne@users.noreply.github.com>
2023-05-16 19:53:33 -0400

    Update Remove-WindowsUserProfile.ps1
    Delete the SID under the registry key "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData"

--- Remove-WindowsUserProfile.ps1
+++ Remove-WindowsUserProfile.ps1
@@ -1,22 +1,37 @@
 function Remove-WindowsProfile {
     [CmdletBinding(SupportsShouldProcess=$true)]
     param (
-        [Parameter(Mandatory=$true,Position=0)]
+        [Parameter(Mandatory=$true, Position=0)]
         [string]$Username
     )
-    $RegKey = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList"
+    
+    $RegKeyProfileList = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList"
+    $RegKeyInstaller = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData"
+    
     $SID = (Get-WmiObject -Class Win32_UserProfile | Where-Object { $_.LocalPath.split('\')[-1] -eq $Username }).SID
+    
     if ($SID) {
         $UserPath = Join-Path -Path "C:\Users" -ChildPath $Username
+        
         if ($PSCmdlet.ShouldProcess($UserPath, "Delete")) {
             Remove-Item -Path $UserPath -Recurse -Force
         }
-        $RegPath = Join-Path -Path $RegKey -ChildPath $SID
-        if ($PSCmdlet.ShouldProcess($RegPath, "Delete")) {
-            Remove-Item -Path $RegPath -Recurse -Force
+        
+        $RegPathProfileList = Join-Path -Path $RegKeyProfileList -ChildPath $SID
+        
+        if ($PSCmdlet.ShouldProcess($RegPathProfileList, "Delete")) {
+            Remove-Item -Path $RegPathProfileList -Recurse -Force
         }
+        
+        $RegPathInstaller = Join-Path -Path $RegKeyInstaller -ChildPath $SID
+        
+        if ($PSCmdlet.ShouldProcess($RegPathInstaller, "Delete")) {
+            Remove-Item -Path $RegPathInstaller -Recurse -Force
+        }
+        
         Write-Output "Profile for $Username has been deleted."
-    } else {
+    }
+    else {
         Write-Output "Profile for $Username was not found."
     }
 }
