99f90ae18a496fed32b8f933d27b618af4ac9150
656d657269636b <29532199+0x3321c@users.noreply.github.com>
2024-02-07 05:28:49 -0500

    Update Remove-WindowsUserProfile.ps1
    Updated with comment based help
    
    Signed-off-by: 656d657269636b <29532199+0x3321c@users.noreply.github.com>

--- Remove-WindowsUserProfile.ps1
+++ Remove-WindowsUserProfile.ps1
@@ -1,64 +1,68 @@
+<#
+.SYNOPSIS
+Delete a user profile
+
+.DESCRIPTION
+This PowerShell script deletes a user profile from a local machine by specifying the username. It removes both the user profile directory and associated registry keys.
+
+.PARAMETER Username
+Specifies the local username (without the domain) whose profile needs to be deleted.
+
+.NOTES
+- Use with caution as deleted profiles and registry keys cannot be recovered.
+- Tested on Windows operating systems.
+
+.ROLE
+User with administrative privileges
+#>
+
 $Username = Read-Host "Enter the local username (without the domain):"
-    
-    $RegKeyProfileList = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList"
-    $RegKeyInstaller = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData"
-    $RegKeyAppxAllUserStore = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore"
-    
-    try {
-        $SID = (Get-WmiObject -Class Win32_UserProfile -ErrorAction Stop | Where-Object { $_.LocalPath.split('\')[-1] -eq $Username }).SID
+
+# Define registry keys
+$RegKeyProfileList = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList"
+$RegKeyInstaller = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData"
+$RegKeyAppxAllUserStore = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore"
+
+try {
+    # Get the SID of the specified user
+    $SID = (Get-WmiObject -Class Win32_UserProfile -ErrorAction Stop | Where-Object { $_.LocalPath.split('\')[-1] -eq $Username }).SID
+}
+catch {
+    Write-Warning "Failed to retrieve SID for user profile, $UserPath"
+}
+
+if ($SID) {
+    $UserPath = Join-Path -Path "C:\Users" -ChildPath $Username
+
+    # Remove user profile directory
+    if (Test-Path($UserPath, "Delete")) {
+        try {
+            Remove-Item -Path $UserPath -Recurse -Force -ErrorAction Stop
+        }
+        catch {
+            Write-Warning "Failed to delete user profile, $UserPath"
+        }
     }
-    catch {
-        Write-Warning "Failed to retrieve SID for user profile, $UserPath"
-      
-    }
-    
-    if ($SID) {
-        $UserPath = Join-Path -Path "C:\Users" -ChildPath $Username
-        
-        if (Test-Path($UserPath, "Delete")) {
+
+    # Remove registry keys associated with the user profile
+    $RegPathProfileList = Join-Path -Path $RegKeyProfileList -ChildPath $SID
+    $RegPathInstaller = Join-Path -Path $RegKeyInstaller -ChildPath $SID
+    $RegPathAppxAllUserStore = Join-Path -Path $RegKeyAppxAllUserStore -ChildPath $SID
+
+    # Remove registry keys
+    foreach ($RegPath in @($RegPathProfileList, $RegPathInstaller, $RegPathAppxAllUserStore)) {
+        if (Test-Path($RegPath, "Delete")) {
             try {
-                Remove-Item -Path $UserPath -Recurse -Force -ErrorAction Stop
+                Remove-Item -Path $RegPath -Recurse -Force -ErrorAction Stop
             }
             catch {
-                Write-Warning "Failed to delete user profile, $UserPath"
+                Write-Warning "Failed to delete registry key, $RegPath"
             }
         }
-        
-        $RegPathProfileList = Join-Path -Path $RegKeyProfileList -ChildPath $SID
-        
-        if (Test-Path($RegPathProfileList, "Delete")) {
-            try {
-                Remove-Item -Path $RegPathProfileList -Recurse -Force -ErrorAction Stop
-            }
-            catch {
-                Write-Warning "Failed to delete registry key, $RegPathProfileList"
-            }
-        }
-        
-        $RegPathInstaller = Join-Path -Path $RegKeyInstaller -ChildPath $SID
-        
-        if (Test-Path($RegPathInstaller, "Delete")) {
-            try {
-                Remove-Item -Path $RegPathInstaller -Recurse -Force -ErrorAction Stop
-            }
-            catch {
-                Write-Warning "Failed to delete registry key, $RegPathInstaller"
-            }
-        }
-        
-        $RegPathAppxAllUserStore = Join-Path -Path $RegKeyAppxAllUserStore -ChildPath $SID
-        
-        if (Test-Path($RegPathAppxAllUserStore, "Delete")) {
-            try {
-                Remove-Item -Path $RegPathAppxAllUserStore -Recurse -Force -ErrorAction Stop
-            }
-            catch {
-                Write-Warning "Failed to delete registry key, $RegPathAppxAllUserStore"
-            }
-        }
-        
-        Write-Output "Profile for $Username has been deleted."
     }
-    else {
-        Write-Output "Profile for $Username was not found."
-    }
+
+    Write-Output "Profile for $Username has been deleted."
+}
+else {
+    Write-Output "Profile for $Username was not found."
+}
