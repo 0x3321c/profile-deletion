b559bf6dd99668e517f7ec8232e01900501d7016
3m321ck 91832n3 <29532199+0x3321c918323@users.noreply.github.com>
2024-02-13 05:05:43 -0500

    Add files via upload
    add auto completion
    
    Signed-off-by: 3m321ck 91832n3 <29532199+0x3321c918323@users.noreply.github.com>

--- Remove-WindowsUserProfile.ps1
+++ Remove-WindowsUserProfile.ps1
@@ -23,11 +23,24 @@ Remove-LocalUser https://learn.microsoft.com/en-gb/powershell/module/microsoft.p
 Delprof2  https://helgeklein.com/free-tools/delprof2-user-profile-deletion-tool 
 #>
 
+ 
+
+class FolderNames : System.Management.Automation.IValidateSetValuesGenerator {
+    [string[]] GetValidValues() {
+        $folderNames = Get-ChildItem -Path "C:\users" -Directory | Select-Object -ExpandProperty Name
+        return $folderNames
+    }
+}
+
+
+
+# Update function,Remove-WindowsUserProfile with argument completion for the parameter, Username
 function Remove-WindowsUserProfile {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
-        [string]$Username
+        [ValidateSet( [folderNames])]
+        [string] $Username
     )
 
     function Write-Log {
@@ -99,77 +112,3 @@ function Remove-WindowsUserProfile {
         Write-Log -Message "Profile for $Username was not found."
     }
 }
-
-
-function Write-Log {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $true)]
-        [string]$Message
-    )
-
-    $LogPath = Join-Path -Path $env:TEMP -ChildPath "Remove-Windows-User-Profile.log"
-    Add-content -Path $LogPath -Value "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $Message"
-}
-
-#$Username = Read-Host "Enter the local username (without the domain):" 
-
-Write-Log -Message "Starting profile deletion process for user: $Username"
-
-# Define registry keys
-$RegKeyProfileList = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList"
-$RegKeyInstaller = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData"
-$RegKeyAppxAllUserStore = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore"
-
-try {
-    # Get the SID of the specified user
-    $SID = (Get-WmiObject -Class Win32_UserProfile -ErrorAction Stop | Where-Object { $_.LocalPath.split('\')[-1] -eq $Username }).SID
-}
-catch {
-    Write-Warning "Failed to retrieve SID for user profile, $UserPath"
-    Write-Log -Message "Failed to retrieve SID for user profile, $Username"
-}
-
-if ($SID) {
-    $UserPath = Join-Path -Path "C:\Users" -ChildPath $Username
-
-    # Remove user profile directory
-    if (Test-Path($UserPath, "Delete")) {
-        try {
-            Remove-Item -Path $UserPath -Recurse -Force -ErrorAction Stop
-            Write-Log -Message "User profile directory deleted: $UserPath"
-        }
-        catch {
-            Write-Warning "Failed to delete user profile, $UserPath"
-            Write-Log -Message "Failed to delete user profile directory: $UserPath"
-        }
-    }
-
-    # Remove registry keys associated with the user profile
-    $RegPathProfileList = Join-Path -Path $RegKeyProfileList -ChildPath $SID
-    $RegPathInstaller = Join-Path -Path $RegKeyInstaller -ChildPath $SID
-    $RegPathAppxAllUserStore = Join-Path -Path $RegKeyAppxAllUserStore -ChildPath $SID
-
-    # Remove registry keys
-    foreach ($RegPath in @($RegPathProfileList, $RegPathInstaller, $RegPathAppxAllUserStore)) {
-        if (Test-Path($RegPath, "Delete")) {
-            try {
-                Remove-Item -Path $RegPath -Recurse -Force -ErrorAction Stop
-                Write-Log -Message "Registry key deleted: $RegPath"
-            }
-            catch {
-                Write-Warning "Failed to delete registry key, $RegPath"
-                Write-Log -Message "Failed to delete registry key: $RegPath"
-            }
-        }
-    }
-
-    Write-Output "Profile for $Username has been deleted."
-    Write-Log -Message "Profile for $Username has been deleted."
-}
-else {
-    Write-Output "Profile for $Username was not found."
-    Write-Log -Message "Profile for $Username was not found."
-}
-
-Remove-WindowsUserProfile -Username usertest
